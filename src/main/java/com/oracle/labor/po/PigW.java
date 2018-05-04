ee288662d28226a738ad8108caa4e4494c5ed339
solid <solid@lee>
2018-05-04 19:54:04 +0800

    添加一个po类

---
+++ src/main/java/com/oracle/labor/po/PigW.java
@@ -0,0 +1,21 @@
+package com.oracle.labor.po;
+
+public class PigW {
+	
+	private String name = "wy";
+	private int age = 30;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
